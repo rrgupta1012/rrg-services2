4c504c6d724e665fbf173bd7cf943ecdd6118565
rrgupta1012 <34778486+rrgupta1012@users.noreply.github.com>
2018-06-11 10:15:03 +0530

    new project

---
+++ my project/javascript/customjs.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$('#php').LineProgressbar({
+  percentage: 65,
+  fillBackgroundColor: '#1abc9c',
+  height: '20px'
+});
+
+	$('#php2').LineProgressbar({
+  percentage: 65,
+  fillBackgroundColor: 'red',
+  height: '20px'
+});
+
+	$('#php3').LineProgressbar({
+  percentage: 65,
+  fillBackgroundColor: 'green',
+  height: '20px'
+});
+
+	$('#php4').LineProgressbar({
+  percentage: 65,
+  fillBackgroundColor: 'blue',
+  height: '20px'
+});
+});
+
+
+new WOW().init();
